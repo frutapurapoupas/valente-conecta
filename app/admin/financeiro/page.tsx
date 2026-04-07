d20f46c001702efa88813c7736f97a3a81e55c70
Arsênio Pereira <frutapurapoupas@gmail.com>
2026-04-07 16:26:18 -0300

    Fix: corrigir export default do FinanceiroPage

--- app/admin/financeiro/page.tsx
+++ app/admin/financeiro/page.tsx
@@ -93,7 +93,7 @@ export default function FinanceiroPage() {
                 </tr>
               ))}
             </tbody>
-          </table>
+           </table>
         </div>
       </div>
     </div>
