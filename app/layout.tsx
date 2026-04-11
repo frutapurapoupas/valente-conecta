3297a2a4652fd9d6378d433c5e4d9d3a251b7a98
Arsênio Pereira <frutapurapoupas@gmail.com>
2026-04-11 03:03:53 -0300

    fix: add icon-192.png e icon-512.png para PWA install correto

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Valente Conecta" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icone.png" />
       </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
