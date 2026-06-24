9a8c9a790c2f846fee9b39841e2080facbc522f3
Arsênio Filho <frutapurapoupas@gmail.com>
2026-06-24 10:00:40 -0300

    fix: vercel.json limpo e remove output standalone e strict mode

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
-  output: 'standalone',
   images: { 
     domains: ['api.qrserver.com', 'valente-conecta.clic.com.br'],
     unoptimized: true 
